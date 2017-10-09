41660fcb9007a8addaeb2b387e5635f870182679
Kelsey Langlois and Megan Olson <megan@>
2017-10-09 09:33:25 -0700

    fix display

--- build/js/app.js
+++ build/js/app.js
@@ -52,7 +52,7 @@ $(document).ready(function() {
     var title = $("#title").val();
     var entry = $("#entry").val();
     var newEntry = new Entry(title, entry);
-    $("ul.entries").append("<li>" + newEntry.title + " " + newEntry.wordCount()  + " " + newEntry.vowels() + " " + newEntry.consonants() + newEntry.getTeaser() + " " + "</li>");
+    $("ul.entries").append("<li>" + newEntry.title + "<br>" + "Word Count: " + newEntry.wordCount() + "<br>" + "Vowels: " + newEntry.vowels() + " Consonants: " + newEntry.consonants() + "<br>" + "<em>\"" +  newEntry.getTeaser() + "\"</em>" + "</li>");
   });
 });
 

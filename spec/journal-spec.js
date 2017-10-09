33fbd9d6599957126979411abcfe088aa1de40ee
Kelsey Langlois and Megan Olson <megan@>
2017-10-09 11:40:30 -0700

    add beforeEach in spec

--- spec/journal-spec.js
+++ spec/journal-spec.js
@@ -1,12 +1,16 @@
 var Entry = require('./../js/entry.js').entryModule;
 
 describe('Entry', function() {
+  var entry;
+
+  beforeEach(function(){
+    entry = new Entry("First Entry", "This is the entry.")
+  });
+
   it("should count the number of words in entry", function(){
-    var entry = new Entry("First Entry", "This is the entry.")
     expect(entry.wordCount()).toEqual(4)
   });
   it("should count the number of vowels in the entry", function(){
-    var entry = new Entry("Another entry", "Entry again.")
     expect(entry.vowels()).toEqual(4)
   });
 });
